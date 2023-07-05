fbfbe08331e286a35baa6112399ea78b79894669
Shubham <sakodiya@grepruby.io>
2023-07-05 16:40:32 +0530

    Added initial configuration.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "default-font-color": "#374151"
+      },
+    },
+  },
+  plugins: [],
+}
+
